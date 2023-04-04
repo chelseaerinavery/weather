890251e5d25bbf2b0d70457127cb8b4a897242be
chelseaerinavery <chelsea.erin.avery@gmail.com>
2023-04-04 12:44:26 -0600

    restyled, updated responsivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,48 @@
-//import logo from './logo.svg';
-import globe from './globe.png';
 import './App.css';
-import {getForecastRequestZip} from '../src/requests/main.js';
+import { getForecastRequestZip } from '../src/requests/main.js';
 import { useState } from 'react';
 import ForecastCard from './components/ForecastCard';
+import videoBg from './assets/videoBg.mp4';
 
 function App() {
- const [forecast, setForecast] = useState(null);
- const [keyword, setKeyword] = useState('');
+  const [forecast, setForecast] = useState(null);
+  const [keyword, setKeyword] = useState('');
 
-const searchZip = async () => {
-  var res = await getForecastRequestZip(keyword)
-  if (res.cod >= '400') {
-    alert("Please type a valid zip code.")
-  } else {
-    setForecast(res)
+  const searchZip = async () => {
+    var res = await getForecastRequestZip(keyword)
+    if (res.cod >= '400') {
+      alert("Please type a valid zip code.")
+    } else {
+      setForecast(res)
+    }
   }
-}
 
   return (
     <div className="App">
-      <header className="App-header"> 
-      <div id="Search">
-      <input 
-        placeholder={"search zip code"}
-        onChange={(e) => setKeyword(e.target.value)}
-      /> 
-      <button onClick={searchZip}>Search</button>
-      </div>
+      <video id={forecast ? "Forecast-video" : "Greeting-video"} src={videoBg} autoPlay loop muted />
+      <header className="App-header">
+        <div id="Search">
+          <input
+            placeholder={"search zip code"}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+          <button onClick={searchZip}>Search</button>
+        </div>
 
-      <img src={globe} className="App-logo" alt="logo" />
-      
-      {!forecast && <span><p id="Greeting">Enter a zip code to receive a forecast detailing weather every 3 hours for the next 24 hours.</p></span>}
+        {!forecast && <span><p id="Greeting">Enter a zip code to receive a forecast detailing weather every 3 hours for the next 24 hours.</p></span>}
 
-      <h1 className="City-name">{forecast?.city.name}</h1>
+        <h1 className="City-name">{forecast?.city.name}</h1>
       </header>
       <div className="Forecast-card-container">
-      {forecast && forecast.list.map((f,i) => {
-        if (i<9) {
-          return <ForecastCard weather={f} city={forecast.city}/>
-        }
-        return null;
-      })}
+        {forecast && forecast.list.map((f, i) => {
+          if (i < 9) {
+            return <ForecastCard weather={f} city={forecast.city} />
+          }
+          return null;
+        })}
       </div>
-      
-    </div>
+
+    </div >
   );
 }
 
